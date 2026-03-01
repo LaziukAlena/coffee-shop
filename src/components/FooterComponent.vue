08aa9056265ed657184d3c55b639081bf606458d
LaziukAlena1988 <miledyhel@mail.ru>
2026-03-01 18:51:43 +0300

    вывод карточек товара через перебор дерективой v-for

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -4,27 +4,17 @@
       <div class="row">
         <div class="col-lg-6 offset-lg-3">
           <ul class="footer d-flex flex-wrap">
-            <li class="footer__item">
-              <router-link :to="links[0].link">
-                <img
-                  :src="require(`@/assets/logo/${links[0].icon}`)"
-                  :alt="links[0].icon"
-                />
-              </router-link>
-            </li>
+            <nav-item :link="links.footer.link" classLink="footer__item">
+              <img
+                :src="require(`@/assets/logo/${links.footer.icon}`)"
+                :alt="links.footer.icon"
+              />
+            </nav-item>
             <nav-item
-              :link="links[1].link"
-              :text="links[1].text"
-              classLink="footer__item"
-            ></nav-item>
-            <nav-item
-              :link="links[2].link"
-              :text="links[2].text"
-              classLink="footer__item"
-            ></nav-item>
-            <nav-item
-              :link="links[3].link"
-              :text="links[3].text"
+              v-for="link in links.other"
+              :key="link.id"
+              :link="link.link"
+              :text="link.text"
               classLink="footer__item"
             ></nav-item>
           </ul>
@@ -32,8 +22,8 @@
       </div>
       <img
         class="beanslogo"
-        :src="require(`@/assets/logo/${links[4].icon}`)"
-        :alt="links[4].icon"
+        src="@/assets/logo/Beans_logo_dark.svg"
+        alt="Beans logo"
       />
     </div>
   </footer>
@@ -46,32 +36,30 @@ export default {
   components: { NavItem },
   data() {
     return {
-      links: [
-        {
+      links: {
+        footer: {
           id: 0,
           link: "/",
           icon: "Logo_black.svg",
         },
-        {
-          id: 1,
-          text: "Our coffee",
-          link: "/our-coffee",
-        },
-        {
-          id: 2,
-          text: "For your pleasure",
-          link: "/goods-page",
-        },
-        {
-          id: 3,
-          text: "Contact us",
-          link: "/contacts",
-        },
-        {
-          id: 4,
-          icon: "Beans_logo_dark.svg",
-        },
-      ],
+        other: [
+          {
+            id: 1,
+            text: "Our coffee",
+            link: "/our-coffee",
+          },
+          {
+            id: 2,
+            text: "For your pleasure",
+            link: "/goods-page",
+          },
+          {
+            id: 3,
+            text: "Contact us",
+            link: "/contacts",
+          },
+        ],
+      },
     };
   },
 };
